ed18b55da52f35fe1ea900bf63d46b98fc3cc91f
Adidtiasmara <mfirmanadid@gmail.com>
2024-10-11 08:31:50 +0700

    Percobaan 3

--- KafeDoWhile18.java
+++ KafeDoWhile18.java
@@ -10,7 +10,7 @@ public class KafeDoWhile18 {
         do {
             System.out.print("Masukkan nama pelanggan (ketik 'batal' untuk keluar): ");
             namaPelanggan = input.nextLine();
-            if (namaPelanggan.equalsIgnoreCase("batal")) {  
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
                 System.out.println("Transaksi dibatalkan");
                 break;
             }
@@ -23,6 +23,7 @@ public class KafeDoWhile18 {
             totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
             System.out.println("Total yang harus dibayar: Rp " + totalHarga);
             input.nextLine();
+
         } while (true);
         System.out.println("Semua transaksi selesai.");
     }
